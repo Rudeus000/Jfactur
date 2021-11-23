21e35a480f96d0337d16ed4428c5442e32034765
washington <washingtoncs15@gmail.com>
2021-11-23 16:23:03 -0500

    update description

--- app/application/views/admin/articulo/listgetiparticulo.php
+++ app/application/views/admin/articulo/listgetiparticulo.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Solutions - caja</title>
+        <title>BFactura - caja</title>
         <meta content="Sistema de gestión de ventas con facturacion electrónica" name="description" />
         <meta content="Coderthemes" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
--- app/application/views/admin/login.php
+++ app/application/views/admin/login.php
@@ -2,7 +2,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Sistema de gestión de ventas con facturacion electrónica</title>
+        <title>BFacturas - SMS</title>
         <meta content="Sistema de gestión de ventas con facturacion electrónica" name="description" />
         <meta content="Coderthemes" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />   
