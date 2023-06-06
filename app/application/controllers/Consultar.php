d622e13dfae03e7fdb8f1e1c04bb667c4680c15e
washingtonaya <77364874+washingtonaya@users.noreply.github.com>
2023-06-06 11:55:18 -0500

    updtae api reniec

--- app/application/controllers/Consultar.php
+++ app/application/controllers/Consultar.php
@@ -4,18 +4,29 @@ error_reporting(E_ALL ^ E_NOTICE);
 $documento  = $_REQUEST['dni'];
 $tipo_doc   = $_REQUEST['tipo_doc'];
 if ($tipo_doc == "2") {
-	$data = file_get_contents("https://bfacturas.000webhostapp.com/datos.php?token=Mmo0RmlzT3EwMG5Ib2JqT2EwTkQ0Zz09&dni=". $documento);
+	// $data = file_get_contents("https://bfacturas.000webhostapp.com/datos.php?token=Mmo0RmlzT3EwMG5Ib2JqT2EwTkQ0Zz09&dni=". $documento);
+	// $info = json_decode($data, true);
+	// $result = $info["dataJson"];
+	// $resultado = $result["persona"];
+	// $datos = array(
+	// 	0 => $info['nuDni'],
+	// 	1 => $info['verificacion'],
+	// 	2 => $info['apellidoPrimero'],
+	// 	3 => $info['apellidoSegundo'],
+	// 	4 => $info['prenombreInscrito'],
+	// 	5 => $info['domicilio'],
+	// 	6 => date('Y-m-d', strtotime($info['fechaNacimiento'])),
+	$data = file_get_contents("https://api.apis.net.pe/v1/dni?numero=" . $documento);
 	$info = json_decode($data, true);
-	$result = $info["dataJson"];
-	$resultado = $result["persona"];
+
 	$datos = array(
-		0 => $info['nuDni'],
-		1 => $info['verificacion'],
-		2 => $info['apellidoPrimero'],
-		3 => $info['apellidoSegundo'],
-		4 => $info['prenombreInscrito'],
-		5 => $info['domicilio'],
-		6 => date('Y-m-d', strtotime($info['fechaNacimiento'])),
+		0 => $info['numeroDocumento'],
+		1 => $info['verificacion'],	
+		2 => $info['apellidoPaterno'],
+		3 => $info['apellidoMaterno'],
+		4 => $info['nombres'],	
+		5 => 'S/N',
+		6 => $info['distrit'],
 
 	);
 	echo json_encode($datos);
