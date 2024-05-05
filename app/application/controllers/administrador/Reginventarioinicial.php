f5dc6e69d9f67bf01fa6d99203ce018198978903
washingtonaya <77364874+washingtonaya@users.noreply.github.com>
2024-05-04 22:29:01 -0500

    update report inventario

--- app/application/controllers/administrador/Reginventarioinicial.php
+++ app/application/controllers/administrador/Reginventarioinicial.php
@@ -297,7 +297,7 @@ class Reginventarioinicial extends CI_Controller
 	function getInventarioInicialReporteseries()
 {
     // Obtener la fecha actual y restar un año
-    $fecha_limite = date('Y-m-d', strtotime('-1 year'));
+    // $fecha_limite = date('Y-m-d', strtotime('-1 year'));
 
     $this->db->select('p.*, nomb_almacen, serie_descripcion, cod_comp, ps.cod_vent AS codigo_venta, serie_estado, histcompstock_serie, fecha_registro,
                         (CASE WHEN serie_estado = "N" THEN v.fecha_vent ELSE NULL END) AS fecha_venta');
@@ -329,14 +329,8 @@ class Reginventarioinicial extends CI_Controller
         $this->db->like('ps.cod_almacen', $almacen);
     }
 
-     // Filtrar por fecha de registro
-	 $this->db->where('p.fecha_registro >=', $fecha_limite);
-
-	 // Filtrar por fecha de venta
-	 $this->db->group_start();
-	 $this->db->where('v.fecha_vent >=', $fecha_limite);
-	 $this->db->or_where('v.fecha_vent IS NULL');
-	 $this->db->group_end();
+    // Limitar la fecha de registro a un año atrás desde la fecha actual
+    // $this->db->where('fecha_vent >=', $fecha_limite);
 
     $this->db->distinct();
 
