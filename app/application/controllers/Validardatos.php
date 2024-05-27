a4ef21fdfe4b9014636219decb47e266b5681bb4
washingtonaya <77364874+washingtonaya@users.noreply.github.com>
2024-05-27 14:58:32 -0500

    update token datos

--- app/application/controllers/Validardatos.php
+++ app/application/controllers/Validardatos.php
@@ -17,7 +17,7 @@ public function validarDocumento()
     {
 $documento = $_REQUEST['dni'];
 $tipo_doc = $_REQUEST['tipo_doc'];
-$token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c3VhcmlvIjoiZGVudGFsc2FjIiwiZXhwIjoxNzE2Njc4ODc1fQ.3EraTOofjlQ82ohhkrUtZqf17M4b1wolKRykDQoxUzc';
+$token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c3VhcmlvIjoiZGVudGFsc2FjIiwiZXhwIjoxNzE5NTE4MjE2fQ.Re9tD-LrbwhynYUAmaADFCyCcEw97tHgVozPPqrH4vU';
 
 
 if ($tipo_doc == "2") {
